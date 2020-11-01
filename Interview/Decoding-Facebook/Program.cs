953fa0f55f323778165cc30b741e7321abace154
Azam Azam <mdazam@microsoft.com>
2020-11-01 07:06:17 -0800

    first commit

---
+++ Interview/Decoding-Facebook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Decoding_Facebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
