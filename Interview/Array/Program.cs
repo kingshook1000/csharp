d309394796dde96d4c36fc59a0c87a57b355a6c4
Azam Azam <mdazam@microsoft.com>
2020-12-29 10:25:31 -0800

    commited

---
+++ Interview/Array/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] parties = new int[3][];
+            parties[0] = new int[] { 1, 2, 3 };
+            parties[1] = new int[] { 10, 20 };
+        }
+    }
+}
---
+++ TestConstructor/TestConstructor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TestConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://stackoverflow.com/questions/42997409/instantiating-an-object-but-using-curly-brackets-instead-of-the-default-constru
+            Console.WriteLine("Testing  Instantiation");
+            //Options options = new Options { Dob = "2020-12-12", Name = "Jon" };
+            Type1 obj1 = new Type1(new Options { Dob = "2020-12-12", Name = "Jon" });
+            Type1 obj2 = new Type1(new Options(false) { Dob = "2020-12-12", Name = "Jon" });
+        }
+    }
+
+    public class Type1
+    {
+        Options options;
+        public Type1(Options options)
+        {
+            this.options = options;
+        }
+    }
+
+    public class Options
+    {
+        bool isTest;
+        public string Name { get; set; }
+
+        public string Dob { get; set; }
+        public Options()
+        { }
+        public Options(bool isTest)
+        {
+            this.isTest = isTest;
+        }
+
+    }
+}
