c4891feb0a60310471eff9c04240035a14dddb18
Azam Azam <mdazam@microsoft.com>
2021-02-05 16:03:56 -0800

    send receive data to event hub

---
+++ DesignPattern/FactoryMethod/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        
+    }
+    
+    public interface IEmplyee
+    {
+        public int GetBonas();
+
+        
+    }
+    public class PermanentEmployee : IEmplyee
+    {
+        int IEmplyee.GetBonas()
+        {
+            return 100;
+        }
+    }
+
+    public class ContractEmployee : IEmplyee
+    {
+        public int GetBonas()
+        {
+            return 10;
+        }
+    }
+
+    public class EmployeeFactory
+    {
+        public IEmplyee CreateEmployee(int employeeType)
+        {
+            if (employeeType == 1)
+            {
+                return new PermanentEmployee();
+            }
+            else if (employeeType == 2)
+            {
+                return new PermanentEmployee();
+            }
+
+            return null;
+
+        }
+    }
+
+}
+
